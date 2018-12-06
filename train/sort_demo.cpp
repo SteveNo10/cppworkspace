36c1910162276c6a3cefbbc1d96d9e3de677d463
SteveNo10 <yongyuan1031@126.com>
2018-12-06 21:00:22 +0800

    finish sort

--- train/sort_demo.cpp
+++ train/sort_demo.cpp
@@ -12,135 +12,197 @@
 
 using namespace std;
 
-void swap_val(int* arr, int idx1, int idx2)
+void swap(int* arr, int idx1, int idx2)
 {
     int tmp = arr[idx1];
     arr[idx1] = arr[idx2];
     arr[idx2] = tmp;
 }
 
-int selection_sort(int* arr, int len)
+void selection_sort(int* arr, int len)
 {
-    if(NULL == arr)
-    {
-        return -1;
-    }
-
-    if(len < 2)
-    {
-        return 0;
-    }
-
     for(int i = 0; i < len - 1; ++i)
     {
         int min_idx = i;
-        for(int j = i + 1; j < len; ++j)
+        for(int j = i; j < len; ++j)
         {
-            if(arr[j] < arr[min_idx])
+            if(arr[min_idx] > arr[j])
             {
                 min_idx = j;
             }
         }
 
-        if(i != min_idx)
+        if(min_idx != i)
         {
-            swap_val(arr, i, min_idx);
+            swap(arr, i, min_idx);
         }
     }
-
-    return 0;
 }
 
-int bubble_sort(int* arr, int len)
+void bubble_sort(int* arr, int len)
 {
-    if(NULL == arr)
-    {
-        return -1;
-    }
-
-    if(len < 2)
-    {
-        return 0;
-    }
-
-    for(int i = len - 1; i > 0; --i)
+    for(int i = len-1; i > 0; --i)
     {
         for(int j = 0; j < i; ++j)
         {
-            if(arr[j] > arr[j + 1])
+            if(arr[j] > arr[j+1])
             {
-                swap_val(arr, j, j + 1);
+                swap(arr, j, j+1);
             }
         }
     }
-
-    return 0;
 }
 
-int insertion_sort(int* arr, int len)
+void insertion_sort(int* arr, int len)
 {
-    if(NULL == arr)
-    {
-        return -1;
-    }
-
-    if(len < 2)
-    {
-        return 0;
-    }
-
     for(int i = 1; i < len; ++i)
     {
-        for(int j = i; j > 1; --j)
+        for(int j = i; j > 0; --j)
         {
-            if(arr[j] < arr[j - 1])
+            if(arr[j] < arr[j-1])
             {
-                swap_val(arr, j, j - 1);
-            }
-            else
-            {
-                break;
+                swap(arr, j, j-1);
             }
         }
     }
-
-    return 0;
 }
 
-int shell_sort(int* arr, int len)
+void shell_sort(int* arr, int len)
 {
-    if(NULL == arr)
+    for(int gap = len/2; gap > 0; gap/=2)
     {
-        return -1;
-    }
-
-    if(len < 2)
-    {
-        return 0;
-    }
-
-    for(int gap = len/2; gap > 0; gap /= 2)
-    {
-        for(int grp = 0; grp < gap; ++grp)
+        for(int group = 0; group < gap; ++group)
         {
-            for(int i = grp + gap; i < len; i += gap)
+            for(int i = group + gap; i < len; i+=gap)
             {
-                for(int j = i; j > grp; j -= gap)
+                for(int j = i; j > group; j-=gap)
                 {
-                    if(arr[j] < arr[j - gap])
+                    if(arr[j] < arr[j-gap])
                     {
-                        swap_val(arr, j, j - gap);
-                    }
-                    else
-                    {
-                        break;
+                        swap(arr, j, j-gap);
                     }
                 }
             }
         }
     }
+}
 
-    return 0;
+void merge(int* arr, int begin, int mid, int end, int* tmp)
+{
+    int low = begin;
+    int high = mid + 1;
+    int k = begin;
+    while(low <= mid && high <= end)
+    {
+        if(arr[low] < arr[high])
+        {
+            tmp[k++] = arr[low++];
+        }
+        else
+        {
+            tmp[k++] = arr[high++];
+        }
+    }
+
+    while(low <= mid)
+    {
+        tmp[k++] = arr[low++];
+    }
+
+    while(high <= end)
+    {
+        tmp[k++] = arr[high++];
+    }
+
+    for(k = begin; k <= end; ++k)
+    {
+        arr[k] = tmp[k];
+    }
+}
+
+void merge_sort(int* arr, int begin, int end, int* tmp)
+{
+    if(begin >= end)
+    {
+        return;
+    }
+
+    int mid = begin + (end - begin) / 2;
+    merge_sort(arr, begin, mid, tmp);
+    merge_sort(arr, mid+1, end, tmp);
+    merge(arr, begin, mid, end, tmp);
+}
+
+void heap_adjust(int* arr, int n, int len)
+{
+    int max_idx = n;
+    int left = 2 * n + 1;
+    int right = left + 1;
+
+    while(left < len && arr[left] > arr[max_idx])
+    {
+        max_idx = left;
+    }
+
+    while(right < len && arr[right] > arr[max_idx])
+    {
+        max_idx = right;
+    }
+
+    if(max_idx != n)
+    {
+        swap(arr, n, max_idx);
+        heap_adjust(arr, max_idx, len);
+    }
+}
+
+void heap_build(int* arr, int len)
+{
+    for(int i = len / 2; i >= 0; --i)
+    {
+        heap_adjust(arr, i, len);
+    }
+}
+
+void heap_sort(int* arr, int len)
+{
+    heap_build(arr, len);
+
+    for(int i = len - 1; i > 0; --i)
+    {
+        swap(arr, 0, i);
+        heap_adjust(arr, 0, i);
+    }
+}
+
+void quick_sort(int* arr, int begin, int end)
+{
+    if(begin >= end)
+    {
+        return;
+    }
+
+    int idx = begin;
+    int left = begin;
+    int right = end;
+    while(left < right)
+    {
+        while(left < right && arr[right] >= arr[idx])
+        {
+            --right;
+        }
+
+        while(left < right && arr[left] <= arr[idx])
+        {
+            ++left;
+        }
+
+        swap(arr, left, right);
+    }
+
+    swap(arr, idx, left);
+    quick_sort(arr, begin, left - 1);
+    quick_sort(arr, left + 1, end);
 }
 
 int set_random_val(int* arr, int len, bool same)
@@ -176,7 +238,7 @@ int set_random_val(int* arr, int len, bool same)
                 int change_idx = random() % len;
                 if(i != change_idx)
                 {
-                    swap_val(arr, i, change_idx);
+                    swap(arr, i, change_idx);
                 }
             }
         }
@@ -211,10 +273,14 @@ int main(int, char**)
         set_random_val(arr, ARR_LEN, (bool)i);
         print_arr(arr, ARR_LEN);
 
-        //selection_sort(arr, ARR_LEN);
+        // selection_sort(arr, ARR_LEN);
         // bubble_sort(arr, ARR_LEN);
         // insertion_sort(arr, ARR_LEN);
-        shell_sort(arr, ARR_LEN);
+        // shell_sort(arr, ARR_LEN);
+        // int tmp[ARR_LEN] = {0};
+        // merge_sort(arr, 0, ARR_LEN - 1, tmp);
+        // heap_sort(arr, ARR_LEN);
+        quick_sort(arr, 0, ARR_LEN -1);
         print_arr(arr, ARR_LEN);
     }
 
